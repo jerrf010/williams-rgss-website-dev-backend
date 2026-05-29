35467adb0a6435962b17be04b7bdc2a061d9852a
zhubenjamin <benjaminzhu2020@gmail.com>
2026-05-28 22:36:21 -0400

    fix image error

--- backend/management/models.py
+++ backend/management/models.py
@@ -19,12 +19,12 @@ class SiteSettings(SingletonModel):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         
-        img = Image.open(self.image.path)
+        img = Image.open(self.stuco_image.path)
         
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.stuco_image.path)
 
     def __str__(self):
         return "Site Configuration"
